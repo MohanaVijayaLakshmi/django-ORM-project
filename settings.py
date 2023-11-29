b7f5e6a6c702d6306bc2137718f592322d4383a6
MohanaVijayaLakshmi <mohanavijayalakshmi.v@gmail.com>
2023-11-28 22:29:05 -0500

    Initital commit

---
+++ settings.py
@@ -0,0 +1,17 @@
+# PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'MjM5NjQtbW9oYW5h',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'standalone',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
